2bbeeb2572f6b8de5bc65cbce69c7383ab9c3ce1
nihal-tej <nihaltejtillu@gmail.com>
2026-01-10 15:39:34 +0530

    interface changes

--- application.py
+++ application.py
@@ -14,31 +14,33 @@ std_model = pickle.load(open('models/scaler.pkl', 'rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predictdata', methods=['POST'])
+@app.route('/predictdata', methods=['POST','GET'])
 def predict_datapoint():
-    Temperature = float(request.form['Temperature'])
-    RH = float(request.form['RH'])
-    Ws = float(request.form['Ws'])
-    Rain = float(request.form['Rain'])
-    FFMC = float(request.form['FFMC'])
-    DMC = float(request.form['DMC'])
-    DC = float(request.form['DC'])
-    ISI = float(request.form['ISI'])
-    Classes = float(request.form['Classes'])
-    Region = float(request.form['Region'])
+  if request.method=="POST":
+     Temperature = float(request.form['Temperature'])
+     RH = float(request.form['RH'])
+     Ws = float(request.form['Ws'])
+     Rain = float(request.form['Rain'])
+     FFMC = float(request.form['FFMC'])
+     DMC = float(request.form['DMC'])
+     DC = float(request.form['DC'])
+     ISI = float(request.form['ISI'])
+     Classes = float(request.form['Classes'])
+     Region = float(request.form['Region'])
 
-    columns = [
+     columns = [
     'Temperature', 'RH', 'Ws', 'Rain',
     'FFMC', 'DMC','DC', 'ISI', 'Classes', 'Region'
      ]
 
-    new_data = pd.DataFrame([[Temperature, RH, Ws, Rain,
+     new_data = pd.DataFrame([[Temperature, RH, Ws, Rain,
                            FFMC, DMC,DC, ISI, Classes, Region]],
                           columns=columns)
-    scaled_data = std_model.transform(new_data)
-    result = ridge_model.predict(scaled_data)
+     scaled_data = std_model.transform(new_data)
+     result = ridge_model.predict(scaled_data)
 
-    return render_template('home.html', result=result[0])
+     return render_template('home.html', result=result[0])
+  return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
